80563f4de29fe8010bfbca6f955ac18dcb1524b6
rafeq abu fiad <rafeqabufiad@rafeqs-MacBook-Pro.local>
2022-08-21 03:30:38 +0300

    first commit

---
+++ lib/widgets/app_drawer.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../screens/orders_screen.dart';
+import '../screens/user_products_screen.dart';
+
+//import '../providers/orders.dart';
+class AppDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          AppBar(
+            title: Text('Hello Friend!'),
+            automaticallyImplyLeading: false,
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.shop),
+            title: Text('Shop'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed('/');
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.payment),
+            title: Text('Orders'),
+            onTap: () {
+              //CartScreen.routeName: (ctx) => CartScreen(),
+              Navigator.of(context).pushReplacementNamed('/orders');
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.edit),
+            title: Text('Manage Products'),
+            onTap: () {
+              Navigator.of(context)
+                  .pushReplacementNamed(UserProductsScreen.routeName);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
